27c7a7fddfe5d244053f967e3bfada07f0fa1837
Alex Gill <alex.gill@depop.com>
2022-10-15 19:17:27 +0100

    Tests

--- src/test/scala/Integration.scala
+++ src/test/scala/Integration.scala
@@ -1,7 +1,29 @@
 import utest._
 
+import scala.collection.mutable.ArrayBuffer
+
+class TestLox extends Lox:
+  val printed = ArrayBuffer.empty[String]
+
+  override def print(message: String): Unit =
+    printed += message
+
 object IntegrationTests extends TestSuite:
 
+  def eval(code: String, expectedOutput: String*) =
+    val lox = new TestLox
+    lox.run(code)
+    assert(lox.printed == expectedOutput)
+
   val tests = Tests {
-    test("Basic expression") {}
+    test("Basic expression") {
+      val input =
+        """
+        var a = 1;
+        var b = 2;
+        print(a+b);
+        """
+
+      eval(input, "3")
+    }
   }
